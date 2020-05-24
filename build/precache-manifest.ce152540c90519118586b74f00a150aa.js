644b53e512423eda80d88b90f737bda4dae445da
bogdanovaO <olya@MacBook-Pro-Olya.local>
2020-05-24 15:30:53 +0300

    test

---
+++ build/precache-manifest.ce152540c90519118586b74f00a150aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d70ea25bb88a2b634d91b88728be787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6307745f8c73ab20bf62",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "be5ffbef42ad434d2e7d",
+    "url": "/static/js/2.a29b9680.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a29b9680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6307745f8c73ab20bf62",
+    "url": "/static/js/main.5eed309a.chunk.js"
+  },
+  {
+    "revision": "10158efe26d043afe7b8",
+    "url": "/static/js/runtime-main.66a840ad.js"
+  }
+]);
