a34d27d14bad5fa10c1385ec8ed3fa335569cca4
bogdanovaO <olya@MacBook-Pro-Olya.local>
2020-08-11 20:16:01 +0300

    Updates

---
+++ precache-manifest.16ef1cb832b9d40be38fa289b4c392c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2ed6feda47927e578bb87ee2706d0c",
+    "url": "/galleryOnReact/index.html"
+  },
+  {
+    "revision": "2b40d82748e7e4e5a443",
+    "url": "/galleryOnReact/static/css/main.2304771f.chunk.css"
+  },
+  {
+    "revision": "bc45610bc44651a63d92",
+    "url": "/galleryOnReact/static/js/2.dc09f54d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/galleryOnReact/static/js/2.dc09f54d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b40d82748e7e4e5a443",
+    "url": "/galleryOnReact/static/js/main.4eb068b6.chunk.js"
+  },
+  {
+    "revision": "70ccdaaa026c37617d60",
+    "url": "/galleryOnReact/static/js/runtime-main.db242620.js"
+  }
+]);
